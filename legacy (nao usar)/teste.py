e5194dc4099220df1272720e8aa2e8f238a3a77c
duduzinho15 <56983681+duduzinho15@users.noreply.github.com>
2025-08-02 22:20:56 -0300

    Delete legacy (nao usar) directory

--- legacy (nao usar)/teste.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("aposta.db")
-cursor = conn.cursor()
-cursor.execute("SELECT COUNT(*) FROM jogos_historicos")
-print("Total de jogos históricos:", cursor.fetchone()[0])
-conn.close()
